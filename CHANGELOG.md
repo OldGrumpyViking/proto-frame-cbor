f3676bf4125057a273d935b786f7bfae255a8e5d
OldGrumpyViking <old.grumpy.viking@hotmail.com>
2022-08-08 21:37:20 +0200

    Removed bitstring as dependency

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,3 +8,4 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 - Added hatch for package management.
 - Setup testing and coverage reporting.
 - Setup type checking, linting and auto formatting.
+- Added BaseFrame abstract class interface.
--- src/proto_frame/frame.py
+++ src/proto_frame/frame.py
@@ -1,41 +1,39 @@
 import abc
 
-import bitstring
-
 
 class BaseFrame(abc.ABC):
     @abc.abstractclassmethod
-    def _from_bitstring(cls):
+    def from_bytes(cls, frame_bytes: bytes):
         pass
 
     @classmethod
-    def from_bytes(cls, frame_bytes: bytes):
-        return cls._from_bitstring(bitstring.Bits(frame_bytes))
-
-    @classmethod
     def from_hex(cls, frame_hex: str):
+        if len(frame_hex) % 2:
+            raise ValueError("Hex string must be even number of chars.")
         if frame_hex.startswith("0x"):
-            return cls._from_bitstring(bitstring.Bits(frame_hex))
-        return cls._from_bitstring(f"0x{bitstring.Bits(frame_hex)}")
+            frame_hex = frame_hex[2:]
+        return cls.from_bytes(bytes.fromhex(frame_hex))
 
     @classmethod
     def from_bin(cls, frame_bin: str):
         if frame_bin.startswith("0b"):
-            return cls._from_bitstring(bitstring.Bits(frame_bin))
-        return cls._from_bitstring(f"0b{bitstring.Bits(frame_bin)}")
+            frame_bin = frame_bin[2:]
+        return cls.from_bytes(int(frame_bin, 2).to_bytes((len(frame_bin) + 7) // 8, byteorder="big"))
 
     @abc.abstractmethod
-    def _to_bitstring(self) -> bitstring.Bits:
-        return bitstring.Bits()
-
     def to_bytes(self) -> bytes:
-        return self._to_bitstring.bytes
+        return b""
 
-    def to_hex(self) -> str:
-        return self._to_bitstring.hex.upper()
+    def to_hex(self, prefix: bool = False) -> str:
+        if prefix:
+            return f"0x{self.to_bytes().hex().upper()}"
+        return self.to_bytes().hex().upper()
 
-    def to_bits(self) -> str:
-        return self._to_bitstring.bin
+    def to_bits(self, prefix: bool = False) -> str:
+        frame_bytes = self.to_bytes()
+        if prefix:
+            return f"0b{format(int.from_bytes(frame_bytes, byteorder='big'), f'0{len(frame_bytes)*8}b')}"
+        return format(int.from_bytes(frame_bytes, byteorder="big"), f"0{len(frame_bytes)*8}b")
 
 
 def Frame(BaseFrame):
