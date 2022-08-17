610637167be40846eb29f70a6aba943530802b28
OldGrumpyViking <old.grumpy.viking@hotmail.com>
2022-08-17 23:00:24 +0200

    Applying namespace fixes

--- src/proto_frame/cbor/frames.py
+++ src/proto_frame/cbor/frames.py
@@ -3,6 +3,7 @@ import proto_frame.core as pf_core
 
 class CborFrame(pf_core.BaseFrame):
     """Base frame for the frame structure."""
+
     def __init__(self, payload: bytes):
         self.payload = payload
 
